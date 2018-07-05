d81a78748e4eedffb8d04c8451f38dd10edf5eb5
tatyana.shvets@itechart-group.com <tatyana.shvets@itechart-group.com>
2018-07-05 19:32:37 +0300

    fix header menu

--- St/App_Start/BundleConfig.cs
+++ St/App_Start/BundleConfig.cs
@@ -9,9 +9,8 @@ namespace St
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.UseCdn = true;
-            var jqueryCDNPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js";
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery", jqueryCDNPath).Include("~/Scripts/lib/jquery/dist/jquery-3.3.1.slim.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js").Include("~/Scripts/lib/jquery/dist/jquery-3.3.1.slim.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/proper", "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/tether").Include("~/Scripts/lib/tether/dist/js/tether.js"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js").Include("~/Scripts/lib/bootstrap/dist/js/bootstrap.js"));
@@ -24,8 +23,8 @@ namespace St
                       "~/Scripts/lib/font-awesome/css/font-awesome.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/styles").Include(
-          "~/Content/points.css",
-          "~/Content/site.css"));
+                      "~/Content/points.css",
+                      "~/Content/site.css"));
 
 
             BundleTable.EnableOptimizations = true;
